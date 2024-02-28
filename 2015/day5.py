1fcc0c5abf314cc60b913be951a10f1bb7d21aa1
Guilherme <none>
2024-02-28 00:07:29 -0300

    day5 -> part 2

--- 2015/day5.py
+++ 2015/day5.py
@@ -4,7 +4,7 @@
 import re
 
 total_nice_strings = 0
-
+total_nice_strings_part_two = 0
 
 def is_nice(string):
     vowels = re.findall(r'[aeiou]', string)
@@ -20,9 +20,22 @@ def is_nice(string):
             and repeated_words_test
             and prohibited_words_test)
 
+def is_nice_part_two(string):
+    repeated_pair = re.findall(r'(\w\w).*\1', string)
+    repeated_pair_test = len(repeated_pair) >= 1
+    
+    letter_between = re.findall(r'(\w)\w\1', string)
+    letter_between_test = len(letter_between) >= 1
+    
+    return (letter_between_test
+            and repeated_pair_test)
+
 with open("inputs/day5.txt", "r") as file:
     for line in file:
         if is_nice(line):
             total_nice_strings += 1
+        if is_nice_part_two(line):
+            total_nice_strings_part_two += 1    
+        
 
-print(total_nice_strings)
+print(f'part 1: {total_nice_strings}\npart2: {total_nice_strings_part_two}')
