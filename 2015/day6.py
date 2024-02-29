aa311602ca6fef3ebc513ce6df9a440d569d6aaf
Guilherme <none>
2024-02-28 23:05:19 -0300

    day6 -> part 2

--- 2015/day6.py
+++ 2015/day6.py
@@ -31,18 +31,40 @@ class Grid:
             self.grid[x0:x1+1, y0:y1+1] = 1
         elif change.endswith('off'):
             self.grid[x0:x1+1, y0:y1+1] = 0
+    
+    def update_grid_part_2(self, change: str, start, end):
         
-    def alter_lights(self, input):
+        x0, y0 = start
+        x1, y1 = end
+        
+        if change == 'toggle':
+            self.grid[x0:x1+1, y0:y1+1] += 2
+        elif change.endswith('on'):
+            self.grid[x0:x1+1, y0:y1+1] += 1
+        elif change.endswith('off'):
+            self.grid[x0:x1+1, y0:y1+1] = np.vectorize(lambda x: max(0, x-1))(
+                self.grid[x0:x1+1, y0:y1+1]
+            )
+        
+    def alter_lights(self, input, part_1=True):
         change, start, end = self.parse_prompt(input)
-        self.update_grid(change, start, end)
+        if part_1:
+            self.update_grid(change, start, end)
+        else:
+            self.update_grid_part_2(change, start, end)
         
     def get_total_lights_on(self):
         return np.sum(self.grid, dtype=np.int32)
         
 
 grid = Grid(1000)
+grid_part_2 = Grid(1000)
 with open('inputs/day6.txt', 'r') as file:
     for line in file:
         grid.alter_lights(line)
+        grid_part_2.alter_lights(line, False)
 print(f'part 1: {grid.get_total_lights_on()}')
+print(f'part 2: {grid_part_2.get_total_lights_on()}')
+
+
 
