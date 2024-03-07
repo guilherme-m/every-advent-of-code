ef5673e6ccf9d8f6ccea02070ae2bfda7fbae827
Guilherme <none>
2024-03-07 18:06:27 -0300

    day 12 -> part 2

--- 2015/day12.py
+++ 2015/day12.py
@@ -4,14 +4,19 @@
 import json
 
 class NumberFinder:
-    def calculate(self, data):
+    def calculate(self, data, puzzle_part=1):
         total = 0
         try:
             iter(data)
+            if (type(data) == dict 
+                    and puzzle_part == 2 
+                    and 'red' in data.values()):
+                #print(data)
+                return 0
             for i in data:
                 if type(data) == dict:
                     i = data[i]
-                total += self.calculate(i)
+                total += self.calculate(i, puzzle_part)
             return total
         except:
             if type(data) == int:
@@ -23,4 +28,5 @@ with open('inputs/day12.txt', 'r') as file:
     json = json.load(file)
 
 num_finder = NumberFinder()
-print(num_finder.calculate(json))
+print(f'part 1: {num_finder.calculate(json)}')
+print(f'part 2: {num_finder.calculate(json, puzzle_part=2)}')
