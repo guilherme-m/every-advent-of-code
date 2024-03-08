6c1d145756703b7cf42a926bf6841ee8984b1c00
Guilherme <none>
2024-03-08 00:28:33 -0300

    day 1 -> part 2

--- 2015/day1.py
+++ 2015/day1.py
@@ -6,11 +6,14 @@ with open('inputs/day1.txt', 'r') as file:
     input = file.read()
     
 floor = 0
+floor_timeline = []
 
 for ch in input:
     if ch == '(':
         floor += 1
     else:
         floor -= 1
+    floor_timeline.append(floor)
         
 print(floor)
+print(floor_timeline.index(-1) + 1)
