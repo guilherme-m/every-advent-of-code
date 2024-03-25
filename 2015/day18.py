a1f808e49295b83d27a064076da2039b397239de
Guilherme <none>
2024-03-25 00:12:13 -0300

    day-18: part 1

---
+++ 2015/day18.py
@@ -0,0 +1,36 @@
+# AoC - 2015 - Day 18
+# Guilherme M.
+# 2024-03-25
+import numpy as np
+
+class Grid:
+    def __init__(self, grid:np.array):
+        self.grid = np.copy(grid)
+    
+    def change_lights(self):
+        new_grid = np.copy(self.grid)
+        for i in range(len(self.grid)):
+            for j in range(len(self.grid[i])):
+                neighbors = self.grid[max(0,i-1):i+2, max(0,j-1):j+2]
+                if self.grid[i,j] == '.':
+                    if np.count_nonzero(neighbors == '#') == 3:
+                        new_grid[i,j] = '#'
+                else:
+                    if not 2 <= np.count_nonzero(neighbors == '#') - 1 <= 3:
+                        new_grid[i,j] = '.'
+        self.grid = new_grid
+                    
+                    
+    
+    def get_lights_on(self):
+        return np.count_nonzero(self.grid == '#')
+    
+grid_txt = open('inputs/day18.txt', 'r').read().split('\n')
+grid_txt = [list(line) for line in grid_txt]
+grid_txt = np.array(grid_txt)
+
+grid = Grid(grid_txt)
+steps = 100
+for _ in range(steps):
+    grid.change_lights()
+print(grid.get_lights_on())
