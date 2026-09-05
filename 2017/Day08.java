7b853b71ba8ccd054283e86d0ce13442e19a71e7
guilherme-m <guilherme.maiabatista@gmail.com>
2026-09-05 01:28:42 -0300

    2017::day-8 part 2

--- 2017/Day08.java
+++ 2017/Day08.java
@@ -7,6 +7,7 @@ public class Day08 implements Day {
 
     public void run() {
         partOne();
+        partTwo();
     }
 
     private List<RegisterProgram.Instruction> readInput() {
@@ -43,73 +44,95 @@ public class Day08 implements Day {
         input.forEach(i -> rp.addInstruction(i));
 
         int max = rp.registers
-                .entrySet()
+                .values()
                 .stream()
-                .max((e1, e2) -> e1.getValue().compareTo(e2.getValue()))
-                .get()
-                .getValue();
+                .mapToInt(Integer::intValue)
+                .max()
+                .getAsInt();
 
-        System.out.println(max);
+        System.out.println("Part 1: " + max);
     }
 
-}
+    private void partTwo() {
 
-class RegisterProgram {
+        var input = readInput();
 
-    public final Map<String, Integer> registers = new HashMap<>();
+        RegisterProgram rp = new RegisterProgram();
 
-    public static record Instruction(
-            String register1,
-            String operation,
-            Integer operationNumber,
-            String register2,
-            String test,
-            Integer testNumber) {
+        input.forEach(i -> rp.addInstruction(i));
+
+        System.out.println("Part 2: " + rp.max);
 
     }
 
-    private void process(Instruction i) {
+    static class RegisterProgram {
 
-        if (doTest(i)) {
+        public final Map<String, Integer> registers = new HashMap<>();
 
-            doOperation(i);
+        public int max;
+
+        public static record Instruction(
+                String register1,
+                String operation,
+                Integer operationNumber,
+                String register2,
+                String test,
+                Integer testNumber) {
 
         }
 
-    }
+        private void process(Instruction i) {
 
-    private boolean doTest(Instruction i) {
-        Integer registerValue = registers.getOrDefault(i.register2(), 0);
-        return switch (i.test()) {
-            case "==" -> registerValue.intValue() == i.testNumber().intValue();
-            case ">" -> registerValue.intValue() > i.testNumber().intValue();
-            case ">=" -> registerValue.intValue() >= i.testNumber().intValue();
-            case "<=" -> registerValue.intValue() <= i.testNumber().intValue();
-            case "<" -> registerValue.intValue() < i.testNumber().intValue();
-            case "!=" -> registerValue.intValue() != i.testNumber().intValue();
-            case null, default -> throw new RuntimeException("Teste invalido");
+            if (doTest(i)) {
 
-        };
-    }
+                doOperation(i);
 
-    private void doOperation(Instruction i) {
+            }
 
-        Integer increment = i.operationNumber();
-
-        if (i.operation().equals("dec")) {
-            increment = -increment;
-        } else if (!i.operation().equals("inc")) {
-            throw new RuntimeException("Operacao invalida");
         }
 
-        registers.merge(i.register1(), increment, (v1, v2) -> v1 + v2);
+
+        private boolean doTest(Instruction i) {
+            Integer registerValue = registers.getOrDefault(i.register2(), 0);
+            return switch (i.test()) {
+                case "==" -> registerValue.intValue() == i.testNumber().intValue();
+                case ">" -> registerValue.intValue() > i.testNumber().intValue();
+                case ">=" -> registerValue.intValue() >= i.testNumber().intValue();
+                case "<=" -> registerValue.intValue() <= i.testNumber().intValue();
+                case "<" -> registerValue.intValue() < i.testNumber().intValue();
+                case "!=" -> registerValue.intValue() != i.testNumber().intValue();
+                case null, default -> throw new RuntimeException("Teste invalido");
+
+            };
+        }
+
+        private void doOperation(Instruction i) {
+
+            Integer increment = i.operationNumber();
+
+            if (i.operation().equals("dec")) {
+                increment = -increment;
+            } else if (!i.operation().equals("inc")) {
+                throw new RuntimeException("Operacao invalida");
+            }
+
+            Integer value = registers.merge(i.register1(), increment, (v1, v2) -> v1 + v2);
+
+            if (value > max) {
+                max = value;
+            }
+
+        }
+
+       
+
+        public void addInstruction(Instruction i) {
+            registers.putIfAbsent(i.register1(), 0);
+            registers.putIfAbsent(i.register2(), 0);
+            process(i);
+
+        }
+
 
     }
-
-    public void addInstruction(Instruction i) {
-
-        process(i);
-
-    }
-
 }
