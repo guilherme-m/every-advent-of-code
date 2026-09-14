9d5e7e372e3bcdd4ed286f36ee85a64671f8dec7
guilherme-m <guilherme.maiabatista@gmail.com>
2026-09-14 16:17:17 -0300

    2017::day-10 part 2

--- 2017/Day10.java
+++ 2017/Day10.java
@@ -1,11 +1,11 @@
 import java.util.stream.*;
 import java.util.*;
-import java.nio.file.*;
 import java.io.*;
 
 public class Day10 implements Day {
     public void run() {
         partOne();
+        partTwo();
     }
 
     public int[] readInput() {
@@ -29,6 +29,27 @@ public class Day10 implements Day {
 
     }
 
+    public int[] readInputAsChars() {
+
+        File f = new File("inputs/Day10.txt");
+
+        try (
+                var fr = new FileReader(f);
+                var br = new BufferedReader(fr)) {
+
+            String input = br.readLine();
+
+            return input
+                    .chars()
+                    .toArray();
+
+        } catch (IOException ioe) {
+
+            throw new RuntimeException(ioe);
+        }
+
+    }
+
     public void partOne() {
         int[] input = readInput();
 
@@ -36,7 +57,25 @@ public class Day10 implements Day {
 
         knotHash.calculateHash();
 
-        System.out.println("Part 1: " + (knotHash.knots[0]*knotHash.knots[1]));
+        System.out.println("Part 1: " + (knotHash.knots[0] * knotHash.knots[1]));
+    }
+
+    public void partTwo() {
+
+        int[] input = readInputAsChars();
+
+        input = IntStream.concat(
+                Arrays.stream(input),
+                Arrays.stream(new int[] { 17, 31, 73, 47, 23 })).toArray();
+
+        var knotHash = new KnotHash(input);
+
+        for (int i = 0; i < 64; i++) {
+            knotHash.calculateHash();
+        }
+
+        System.out.println(knotHash.getDenseHash());
+
     }
 
     private static class KnotHash {
@@ -105,6 +144,31 @@ public class Day10 implements Day {
 
             knots[i] = knots[j];
             knots[j] = tmp;
+
+        }
+
+        public String getDenseHash() {
+
+            List<Integer> list = new ArrayList<>();
+
+            for (var i = 0; i <= KNOTS_SIZE - 16; i += 16) {
+
+                int xorInt = knots[i];
+
+                for (var j = 1; j < 16; j++) {
+
+                    xorInt = xorInt ^ knots[i + j];
+
+                }
+
+                list.add(xorInt);
+
+            }
+
+            return list
+                    .stream()
+                    .map(x -> String.format("%02x", x))
+                    .collect(Collectors.joining());
 
         }
 
